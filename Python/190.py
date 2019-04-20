4de30fa391cc4d13f9743b6802311291a7347bfc
EpsilonHF <epsilonhf@outlook.com>
2019-04-20 12:09:52 +0800

    add new

---
+++ Python/190.py
@@ -0,0 +1,27 @@
+"""
+Reverse bits of a given 32 bits unsigned integer.
+
+
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 
+represents the unsigned integer 43261596, so return 964176192 
+which its binary representation is 00111001011110000010100101000000.
+"""
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+
+        bit = 0
+        rev = 0
+        while n:
+            rev = rev*2 + n%2
+            n //= 2
+            bit += 1
+
+        return rev << (32-bit)
---
+++ Python/191.py
@@ -0,0 +1,27 @@
+"""
+Write a function that takes an unsigned integer and return 
+the number of '1' bits it has (also known as the Hamming weight).
+
+
+
+Example 1:
+
+Input: 00000000000000000000000000001011
+Output: 3
+Explanation: 
+The input binary string 00000000000000000000000000001011 has 
+a total of three '1' bits.
+"""
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ans = 0
+        while n:
+            ans += n % 2
+            n //= 2
+
+        return ans
