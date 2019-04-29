550c1d4419819a0a24d9a2e14e7a8ae61ea435de
EpsilonHF <epsilonhf@outlook.com>
2019-04-29 19:33:00 +0800

    add new

---
+++ Python/67.py
@@ -0,0 +1,24 @@
+"""
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only 
+characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+"""
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if len(a) == 0:
+            return b
+        if len(b) == 0:
+            return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'
+        if a[-1] == '1' or b[-1] == '1':
+            return self.addBinary(a[:-1], b[:-1]) + '1'
+        else:
+            return self.addBinary(a[:-1], b[:-1]) + '0'
